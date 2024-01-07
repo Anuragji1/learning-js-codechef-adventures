39db80b927abc0d6f89b9071221b266a1a8b8e70
Anurag Kumar <140825596+Anuragji1@users.noreply.github.com>
2024-01-07 20:12:01 +0530

    Create if_else.js

---
+++ if_else.js
@@ -0,0 +1,28 @@
+Statement) 
+In the given program that takes a number as input and checks if it is positive, negative, or zero.
+We have displayed an appropriate message based on the input.
+Your task is to put the correct relational operators. <, >, or =.
+  /* Update the '_' in the code below to solve the problem*/
+
+var number = -5;
+
+if (number _ 0) {
+  console.log("The number is positive.");
+} else if (number _ 0) {
+  console.log("The number is negative.");
+} else {
+  console.log("The number is zero.");
+}
+
+--------------------------------------------
+SOLUTION = // Corrected code to check the positivity of a number
+
+var number = -5;
+
+if (number > 0) {
+  console.log("The number is positive.");
+} else if (number < 0) {
+  console.log("The number is negative.");
+} else {
+  console.log("The number is zero.");
+}
