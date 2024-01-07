dceb389cb023f8e6785a8eec7e24fe4002519e21
Anurag Kumar <140825596+Anuragji1@users.noreply.github.com>
2024-01-07 20:26:15 +0530

    Update BMI_Calculator.js

--- BMI_Calculator.js
+++ BMI_Calculator.js
@@ -52,3 +52,36 @@ weight = [30, 55, 90, 102, 65]                      20.20 Normal Weight
                                                     27.78 Overweight
                                                     39.84 Obese
                                                     21.22 Normal Weight
+
+---------------------------------------------------------------------------------
+SOLUTION = const height = [1.70, 1.65, 1.80, 1.60, 1.75];
+const weight = [30, 55, 90, 102, 65];
+
+for (let i = 0; i < height.length; i++) {
+
+  const bmi = weight[i] / (height[i] * height[i]);
+
+  let bmiCategory;
+  if (bmi < 18.5) {
+    bmiCategory = "Underweight";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    bmiCategory = "Normal Weight";
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    bmiCategory = "Overweight";
+  } else {
+    bmiCategory = "Obese";
+  }
+
+  console.log(bmi.toFixed(2) + " " + bmiCategory);
+}
+
+-----------------------------------------------------------------------------------
+ Sample Input
+height = [1.70, 1.65, 1.80, 1.60, 1.75]
+weight = [30, 55, 90, 102, 65]
+Your Output
+10.38 Underweight
+20.20 Normal Weight
+27.78 Overweight
+39.84 Obese
+21.22 Normal Weight
