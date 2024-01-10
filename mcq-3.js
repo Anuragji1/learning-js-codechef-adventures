75aba8fcb7c389bf1e7117cf8e8ce57d02857d7a
Anurag Kumar <140825596+Anuragji1@users.noreply.github.com>
2024-01-10 20:07:01 +0530

    Create mcq-3.js

---
+++ mcq-3.js
@@ -0,0 +1,15 @@
+**MCQ-3**
+-------
+Statement) What does the displayContents() function do in the code you did in the previous problem?
+Select one of the following options.
+Clears the contents of container1 and container2.
+Creates content items and appends them to container1 and container2.
+Adds a delete button to each content item.
+Calls another function which creates content items and then take that content item and appends it to container1 and container2.
+
+-------------------------------------------------------------------------------------------------------------------------------
+SOLUTION = Correct Answer:
+
+Calls another function which creates content items and then take that content item and appends it to container1 and container2.
+Explanation:
+The displayContents() function is responsible for calling the createContentElement function which creates content items based on the provided data and then appending those content items to container1 and container2. 
